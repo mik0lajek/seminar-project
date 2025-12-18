dd08ce45049a7b59f9ca3231da3de25be41d1715
Mikołaj Sosiński <164782094+mik0lajek@users.noreply.github.com>
2025-12-18 15:57:48 +0100

    Add recursion merge-sort

---
+++ sorting/merge-sort_rec.py
@@ -0,0 +1,68 @@
+#Source: https://www.geeksforgeeks.org/dsa/merge-sort/
+import time
+
+
+def merge(arr, left, mid, right):
+    n1 = mid - left + 1
+    n2 = right - mid
+
+    # Create temp arrays
+    L = [0] * n1
+    R = [0] * n2
+
+    # Copy data to temp arrays L[] and R[]
+    for i in range(n1):
+        L[i] = arr[left + i]
+    for j in range(n2):
+        R[j] = arr[mid + 1 + j]
+
+    i = 0
+    j = 0
+    k = left
+
+    # Merge the temp arrays back
+    # into arr[left..right]
+    while i < n1 and j < n2:
+        if L[i] <= R[j]:
+            arr[k] = L[i]
+            i += 1
+        else:
+            arr[k] = R[j]
+            j += 1
+        k += 1
+
+    # Copy the remaining elements of L[],
+    # if there are any
+    while i < n1:
+        arr[k] = L[i]
+        i += 1
+        k += 1
+
+    # Copy the remaining elements of R[],
+    # if there are any
+    while j < n2:
+        arr[k] = R[j]
+        j += 1
+        k += 1
+
+
+def merge_sort(arr, left, right):
+    if left < right:
+        mid = (left + right) // 2
+
+        merge_sort(arr, left, mid)
+        merge_sort(arr, mid + 1, right)
+        merge(arr, left, mid, right)
+
+
+# Driver code
+if __name__ == "__main__":
+    arr = [38, 27, 43, 10, 1, 93, 34, 34, 5, 143, 54, 3764, 342, 7868, 2423]
+
+    start = time.perf_counter()
+    merge_sort(arr, 0, len(arr) - 1)
+    end = time.perf_counter()
+
+    for i in arr:
+        print(i, end=" ")
+    print("\nSorted in: " + str(end-start))
